c23646da2099493ff3704ba15ff0b4ab42c1860f
Sarah <sarahkulsoom18@gmail.com>
2023-07-01 13:46:04 +0530

    Valid Parenthesis

---
+++ ValidParenthesis.java
@@ -0,0 +1,31 @@
+package walmart;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == ')') {
+                if (st.isEmpty() || st.peek() != '(') {
+                    return false;
+                }
+                st.pop();
+            } else if (c == ']') {
+                if (st.isEmpty() || st.peek() != '[') {
+                    return false;
+                }
+                st.pop();
+            } else if (c == '}') {
+                if (st.isEmpty() || st.peek() != '{') {
+                    return false;
+                }
+                st.pop();
+            } else {
+                st.push(c);
+            }
+
+        }
+        return st.isEmpty();
+    }
+}
