260113018e926f039d7773ee71b08ac3ed49f2d5
Sarah <sarahkulsoom18@gmail.com>
2023-07-05 14:59:48 +0530

    Level Order Traversal

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,33 @@
+package walmart;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> curLevel = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = q.poll();
+                curLevel.add(cur.val);
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+            }
+            res.add(curLevel);
+        }
+        return res;
+    }
+}
