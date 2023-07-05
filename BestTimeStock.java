8e1c72adaa2dc3f1fae7fbf1895623a75c64352c
Sarah <sarahkulsoom18@gmail.com>
2023-07-05 10:38:57 +0530

    Best Time To Auy And Sell Stock

---
+++ BestTimeStock.java
@@ -0,0 +1,12 @@
+package walmart;
+
+public class BestTimeStock {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0, minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+}
